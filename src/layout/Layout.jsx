74be06c5cd8ae0391a309baf3ae75981aedc7511
guiss26 <guissella584@gmail.com>
2025-09-17 16:26:32 -0700

    feat(home): add logo image and button in Home.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-
+import "./Layout.css"
 const Layout = () => {
     return (
         <>
