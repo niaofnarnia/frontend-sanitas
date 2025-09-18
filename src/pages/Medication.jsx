c59a57036710c94c322d6f7bd037ca14747e918c
guiss26 <guissella584@gmail.com>
2025-09-18 01:38:16 -0700

    style(medications): adjust CSS classes for color legend (#7)

--- src/pages/Medication.jsx
+++ src/pages/Medication.jsx
@@ -1,9 +1,30 @@
 import { Link } from 'react-router-dom'
+import './Medication.css'
+import { useState, useEffect } from 'react'
+import MedicationList from '../components/MedicationList'
 
 const Medication = () => {
     return (
         <>
-            <div className="bex-medication"></div>
+            <div className="legend">
+                <div className="legend-item cronic">
+                    <div className="legend-color"></div>
+                    <span className='legend-text'>Crónico</span>
+                </div>
+                <div className="legend-item eventual">
+                    <div className="legend-color"></div>
+                    <span className='legend-text'>Eventual</span>
+                </div>
+                <div className="legend-item reciente">
+                    <div className="legend-color"></div>
+                    <span className='legend-text'>Reciente</span>
+                </div>
+            </div>
+
+            <div className="box-medication">
+                <h2 className="medication-title">Medicamentos</h2>
+                <MedicationList></MedicationList>
+            </div>
         </>
     )
 }
