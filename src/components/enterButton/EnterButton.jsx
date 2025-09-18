890e422f67b678a359b072457042e0e44f689355
RubenCuevas79 <sr_ruben2000@yahoo.es>
2025-09-18 10:28:23 +0200

    create modifique  button

--- src/components/enterButton/EnterButton.jsx
+++ src/components/enterButton/EnterButton.jsx
@@ -1,10 +1,6 @@
 import './EnterButton.css'
 <imput className="EnterButton" type="button"></imput>
-const EnterButton = ({ texto = "Entrar" }) => {
 
-  return () <button className="StartButton">{texto}</button>;
-
-};
 
 export default EnterButton;
 
